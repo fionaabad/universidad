a8bff3ad93d7af85c6c1f3054b74231927f6ff07
Fiona Abad <fionaabad13@gmail.com>
2025-10-02 18:30:11 +0200

    primera parte DAOcarrera

--- DAOcarrera.py
+++ DAOcarrera.py
@@ -2,11 +2,20 @@ import mysql.connector
 from carrera import Carrera
 
 class CarreraDAO:
-    def __init__(self, conn):
-        self.__conn = conn
+    def __init__(self):
+        self.__conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database="universidad"
+        )
         
     def create(self, c):
-        pass
+        cur = self.__conn.cursor()
+        sql = "INSERT INTO carreras (nombre, duracion, id) VALUES (%s, %s, %s)"
+        cur.execute(sql, (c.nombre, c.duracion, c.id_))
+        self.__conn.commit()
+        cur.close()
     
     def read(self, id_):
         pass
@@ -17,7 +26,7 @@ class CarreraDAO:
     def delete(self, id_):
         pass
     
-    def select(self):
+    def fetchall(self):
         pass
 
 
