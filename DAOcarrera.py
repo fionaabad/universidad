abb4b12a4df64bc723f9d1684713a987e656cdc0
Carlos Valero <carlosvalerosala2020@gmail.com>
2025-10-02 18:56:12 +0200

    Funcion read

--- DAOcarrera.py
+++ DAOcarrera.py
@@ -8,8 +8,19 @@ class CarreraDAO:
     def create(self, c):
         pass
     
-    def read(self, id_):
-        pass
+    def read(self, nombre = ""):
+        query = ""
+
+        if nombre == "":
+            query = "SELECT * FROM carrera"
+            GetCursor().execute(query)
+
+        else:
+            query = "SELECT * FROM carrera WHERE nombre LIKE %s"
+            values = [nombre]
+            GetCursor().execute(query, values)
+            
+        return GetCursor().fetchall()
     
     def update(self, c):
         pass
