249349579ebf8877bf47ed70d8e682ae0d469871
Carlos Valero <carlosvalerosala2020@gmail.com>
2025-10-02 19:50:04 +0200

    Optimizar bucle

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ def opcion_borrar(dao, nombre):
     
 dao = CarreraDAO()   
 
-while True:
+continuar = True
+
+while continuar:
     print(mostrar_menu())
     opcion = input("Elige opción: ").strip()
     
@@ -42,7 +44,7 @@ while True:
         pass
     
     elif opcion == '0':
-        pass
+        continuar = False
     
     else:
         print("Opción no válida")
