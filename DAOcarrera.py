327a85f5093a4a31161ba1ac325bcc8309208436
Fiona Abad <fionaabad13@gmail.com>
2025-10-02 18:09:14 +0200

    base DAOCarrera

---
+++ DAOcarrera.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from carrera import Carrera
+
+class CarreraDAO:
+    def __init__(self, conn):
+        self.__conn = conn
+        
+    def create(self, c):
+        pass
+    
+    def read(self, id_):
+        pass
+    
+    def update(self, c):
+        pass
+    
+    def delete(self, id_):
+        pass
+    
+    def select(self):
+        pass
+
+
